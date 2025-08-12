6bbc1c99d9ba6929172feb24604149055b40c856
aoreilly4 <aoreilly@secretariat-intl.com>
2025-08-12 14:29:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,4 @@ import os
 
 df = pd.DataFrame({'TEST':['1','2']})
 
-# Create the Output directory if it doesn't exist
-os.makedirs('Output', exist_ok=True)
-
-print(df)
-
 df.to_csv('Output/output.csv', index=False)
